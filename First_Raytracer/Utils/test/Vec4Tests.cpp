f795ce0b1b5e84268be006a175085e384056901a
Alexander Borgen <alexanderborgen@gmail.com>
2019-03-27 16:11:05 -0700

    Add tests for Vec4

---
+++ First_Raytracer/Utils/test/Vec4Tests.cpp
@@ -0,0 +1,57 @@
+#include "catch.hpp"
+
+#include "../Vec4.h"
+
+SCENARIO("Basic operations between two vectors in R^4")
+{
+	using namespace Utils;
+	SECTION("Addition")
+	{
+		Vec4 expected(3.5f, 2.0f, 1.0f, 10.0f);
+
+		Vec4 first(2.5f, 1.0f, 1.0f, 7.5f);
+		Vec4 second(1.0f, 1.0f, 0.0f, 2.5f);
+		Vec4 result = first + second;
+		REQUIRE(
+			(
+				expected.getX() == result.getX() &&
+				expected.getY() == result.getY() &&
+				expected.getZ() == result.getZ() &&
+				expected.getW() == result.getW() 
+			)
+		);
+	}
+	
+	SECTION("Subtraction")
+	{
+		Vec4 expected(1.5f, 0.0f, 1.0f, 3.5);
+
+		Vec4 first(2.5f, 1.0f, 1.0f, 4.0f);
+		Vec4 second(1.0f, 1.0f, 0.0f, 0.5f);
+		Vec4 result = first - second;
+		REQUIRE(
+			(
+				expected.getX() == result.getX() &&
+				expected.getY() == result.getY() &&
+				expected.getZ() == result.getZ() &&
+				expected.getW() == result.getW()
+			)
+		);
+	}
+
+	SECTION("Equality")
+	{
+		Vec4 first(1.5f, 1.0f, 2.0f, 0.0f);
+		Vec4 second(1.5f, 1.0f, 2.0f, 0.0f);
+		bool objectEquality = first == second;
+		REQUIRE(objectEquality);
+	}
+
+	SECTION("Inequality")
+	{
+		Vec4 first(1.0f, 1.0f, 1.0f, 1.0f);
+		Vec4 second(1.5f, 1.0f, 2.0f, 0.0f);
+		bool objectInequality = first != second;
+		REQUIRE(objectInequality);
+	}
+}
