1adb04e3b35d4285b834b2eb91fba75984a97901
Alexander Borgen <alexanderborgen@gmail.com>
2019-03-27 16:59:11 -0700

    Add tests for Operations

---
+++ First_Raytracer/Utils/test/OperationsTests.cpp
@@ -0,0 +1,46 @@
+#include "catch.hpp"
+
+#include "../Operations.h"
+#include "../Vec3.h"
+#include "../Vec4.h"
+
+SCENARIO("When vectors given are acceptable (in R^3)")
+{
+	using namespace Utils;
+	SECTION("Cross product between two vectors")
+	{
+		Vec3 first(1.0f, 2.0f, 3.0f);
+		Vec3 second(4.0f, 5.0f, 6.0f);
+
+		Vec3 expected(-3.0f, 6.0f, -3.0f);
+		Vec3 result = Operations::cross(first, second);
+		bool objectEquality = expected == result;
+		REQUIRE(objectEquality);
+	}
+
+	SECTION("Dot product between two vectors")
+	{
+		Vec3 first(1.0f, 2.0f, 3.0f);
+		Vec3 second(4.0f, 5.0f, 6.0f);
+
+		int expected = 32;
+		float result = Operations::dot(first, second);
+		REQUIRE(expected == result);
+	}
+
+	SECTION("Normalization of a vector")
+	{
+		Vec3 vector(2.0f, 4.0f, 6.0f);
+		double expectedLength = 1;
+		Vec3 result = Operations::normalize(vector);
+		REQUIRE(expectedLength == result.length());
+	}
+
+	SECTION("Convert from degrees to radians")
+	{
+		float degrees = 60.0f;
+		float expected = 1.0472f;
+		float result = Operations::toRadians(degrees);
+		REQUIRE(expected == result);
+	}
+}
