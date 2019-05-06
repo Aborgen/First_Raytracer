bc2d0a918dabd7e100b02b4db5d48b829b5a03ed
Alexander Borgen <alexanderborgen@gmail.com>
2019-05-06 15:42:06 -0700

    Anything that is RayCapable must implement a generateDirection method

--- First_Raytracer/Raytracer/RayCapable.h
+++ First_Raytracer/Raytracer/RayCapable.h
@@ -4,17 +4,22 @@
 
 #include "../Ray/Ray.h"
 #include "../Utils/Vec3.h"
+#include "../Screen/Screen.h"
 
 namespace Processing
 {
 	class RayCapable
 	{
 	public:
-		virtual Ray castRay(Utils::Vec3 direction);
+		virtual Ray castRay(float rasterX, float rasterY) = 0;
 
 	protected:
 		RayCapable() {};
 		Utils::Vec3 position;
+		float offset;
+
+	private:
+		virtual Utils::Vec3 generateDirection(float rasterX, float rasterY) = 0;
 	};
 }
 
