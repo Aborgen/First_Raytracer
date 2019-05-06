3371ef1f3a7b7d38aec941c628de7f11c004da21
Alexander Borgen <alexanderborgen@gmail.com>
2019-05-06 15:40:12 -0700

    Add Ray class

---
+++ First_Raytracer/Ray/Ray.h
@@ -0,0 +1,24 @@
+#ifndef RAY_H
+#define RAY_H
+#pragma once
+
+#include "../Utils/Vec3.h"
+namespace Processing
+{
+	class Ray
+	{
+	public:
+		Ray() {};
+		Ray(Utils::Vec3 direction, Utils::Vec3 origin);
+		Utils::Vec3 getDirection();
+		void setDirection(float x, float y, float z);
+		Utils::Vec3 getOrigin();
+		void setOrigin(float x, float y, float z);
+
+	private:
+		Utils::Vec3 direction;
+		Utils::Vec3 origin;
+	};
+}
+
+#endif // !RAY_H
---
+++ First_Raytracer/Ray/src/Ray.cpp
@@ -0,0 +1,32 @@
+#include "../Ray.h"
+
+namespace Processing
+{
+	Ray::Ray(Utils::Vec3 direction, Utils::Vec3 origin)
+	{
+		this->direction = direction;
+		this->origin = origin;
+	}
+
+	Utils::Vec3 Ray::getDirection()
+	{
+		return direction;
+	}
+
+	void Ray::setDirection(float x, float y, float z)
+	{
+		Utils::Vec3 direction(x, y, z);
+		this->direction = direction;
+	}
+
+	Utils::Vec3 Ray::getOrigin()
+	{
+		return origin;
+	}
+
+	void Ray::setOrigin(float x, float y, float z)
+	{
+		Utils::Vec3 origin(x, y, z);
+		this->origin = origin;
+	}
+}
