7a0dcf345d1d1e33f1df415bb73f09b079c1849f
Alexander Borgen <alexanderborgen@gmail.com>
2019-05-06 15:35:30 -0700

    Add Sphere class that derives from Shape. Can test intersection of a Ray.

---
+++ First_Raytracer/Geometry/Sphere.h
@@ -0,0 +1,25 @@
+#ifndef SPHERE_H
+#define SPHERE_H
+#pragma once
+
+#include <optional>
+
+#include "Shape.h"
+
+namespace Geometry
+{
+	class Sphere : public Shape
+	{
+	public:
+		Sphere() : Shape(Type::SPHERE) {}
+		Sphere(const Utils::Vec3 &center, float radius, const Processing::MaterialProps &material);
+		std::optional<float> intersect(Processing::Ray &ray);
+
+	private:
+		Utils::Vec3 center;
+		float radius;
+		std::optional<float> findClosestRoot(float a, float b, float c);
+	};
+}
+
+#endif // !SPHERE_H
---
+++ First_Raytracer/Geometry/src/Sphere.cpp
@@ -0,0 +1,55 @@
+#include "../Sphere.h"
+#include "../../Utils/Operations.h"
+
+namespace Geometry
+{
+	Sphere::Sphere(const Utils::Vec3 &center, float radius, const Processing::MaterialProps &material) : Shape(Type::SPHERE)
+	{
+		this->center = center;
+		this->radius = radius;
+		this->material = material;
+		this->transformation = transformation;
+	}
+
+	std::optional<float> Sphere::intersect(Processing::Ray &ray)
+	{
+		using namespace Utils;
+		Vec3 origin = ray.getOrigin();
+		Vec3 direction = ray.getDirection();
+		Vec3 delta = origin - center;
+		// Set up quadratic equation
+		float a = Operations::dot(direction, direction);
+		float b = 2 * Operations::dot(direction, delta);
+		float c = Operations::dot(delta, delta) - std::pow(radius, 2);
+		std::optional<float> intersection = findClosestRoot(a, b, c);
+		if (!intersection.has_value()) {
+			return std::nullopt;
+		}
+
+		return intersection.value();
+	}
+
+	std::optional<float> Sphere::findClosestRoot(float a, float b, float c)
+	{
+		float discriminant = std::pow(b, 2) - 4 * a * c;
+		// There are no real roots
+		if (discriminant < 0) {
+			return std::nullopt;
+		}
+
+		float root;
+		// There is one real root
+		if (discriminant == 0) {
+			root = -0.5 * b / a;
+		}
+		else {
+			float sign = b > 0 ? 1 : -1;
+			float quadratic = -0.5 * (b + sign * sqrt(discriminant));
+			float root1 = quadratic / a;
+			float root2 = c / quadratic;
+			root = root1 < root2 ? root1 : root2;
+		}
+
+		return root;
+	}
+}
