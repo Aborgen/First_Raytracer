68756cf0109eb5e3638986d8119e0ddedb9065a8
Alexander Borgen <alexanderborgen@gmail.com>
2019-05-07 15:02:59 -0700

    Add typesdefs for deques

--- First_Raytracer/Types/LightPtr.h
+++ First_Raytracer/Types/LightPtr.h
@@ -2,10 +2,12 @@
 #define LIGHTPTR_H
 #pragma once
 
+#include <deque>
 #include <memory>
 
 #include "../Light/Light.h"
 
 typedef std::shared_ptr<Processing::Light> LightPtr;
+typedef std::deque<LightPtr> LightDeque;
 
 #endif // !LIGHTPTR_H
--- First_Raytracer/Types/ShapePtr.h
+++ First_Raytracer/Types/ShapePtr.h
@@ -2,10 +2,12 @@
 #define SHAPEPTR_H
 #pragma once
 
+#include <deque>
 #include <memory>
 
 #include "../Geometry/Shape.h"
 
 typedef std::shared_ptr<Geometry::Shape> ShapePtr;
+typedef std::deque<ShapePtr> ShapeDeque;
 
 #endif // !SHAPEPTR_H
