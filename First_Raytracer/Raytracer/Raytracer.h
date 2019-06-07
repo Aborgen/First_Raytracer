ad6d5190c379825188f6ae9a470c26407bb100d8
Alexander Borgen <alexanderborgen@gmail.com>
2019-06-06 17:08:37 -0700

    Extract loop which checks every shape for intersection; Add final color calculations to be done after casting shadow rays toward each light source

--- First_Raytracer/Raytracer/Raytracer.h
+++ First_Raytracer/Raytracer/Raytracer.h
@@ -2,22 +2,35 @@
 #define RAYTRACER_H
 #pragma once
 
+#include "../MaterialProps/MaterialProps.h"
 #include "../Types/LightPtr.h"
 #include "../Types/ShapePtr.h"
 #include "../Utils/ColorTriad.h"
+#include "../Utils/Vec4.h"
 
 namespace Processing
 {
+	struct IntersectionInfo
+	{
+		ShapePtr shape = nullptr;
+		float t = std::numeric_limits<float>::infinity();
+	};
+
 	class Raytracer
 	{
 	public:
 		Raytracer() {};
-		Raytracer(const LightDeque &lights, const ShapeDeque &shapes);
-		Utils::ColorTriad trace(const Ray &ray) const;
+		Raytracer(const LightDeque &lights, const ShapeDeque &shapes) : lights(lights), shapes(shapes), backgroundColor(0.0f, 0.0f, 0.0f) {};
+		Raytracer(const LightDeque &lights, const ShapeDeque &shapes, const Utils::ColorTriad &backgroundColor) : lights(lights), shapes(shapes), backgroundColor(backgroundColor) {};
+		Utils::ColorTriad trace(const Ray &ray);
+
 
 	private:
 		LightDeque lights;
 		ShapeDeque shapes;
+		Utils::ColorTriad backgroundColor;
+		bool traceClosest(const Ray &ray, IntersectionInfo &info);
+		Utils::ColorTriad computeLight(const Utils::Vec3 &direction, const Utils::Vec3 &normal, const Utils::Vec3 &half, const LightPtr &light, const MaterialProps &material);
 	};
 }
 
--- First_Raytracer/Raytracer/src/Raytracer.cpp
+++ First_Raytracer/Raytracer/src/Raytracer.cpp
@@ -1,20 +1,50 @@
 #include <optional>
 
 #include "../Raytracer.h"
-#include "../../MaterialProps/MaterialProps.h"
+#include "../Utils/Operations.h"
 
 namespace Processing
 {
-	Raytracer::Raytracer(const LightDeque &lights, const ShapeDeque &shapes)
+	Utils::ColorTriad Raytracer::trace(const Ray &ray)
 	{
-		this->lights = lights;
-		this->shapes = shapes;
+		IntersectionInfo info;
+		bool intersection = traceClosest(ray, info);
+		if (!intersection) {
+			return backgroundColor;
+		}
+
+		Utils::Vec3 point = ray.specificPoint(info.t);
+		Utils::Vec3 normal = info.shape->normalAtPoint(point);
+		MaterialProps material = info.shape->getMaterial();
+		Utils::ColorTriad finalColor = material.getAmbient() + material.getEmission();
+		for (const LightPtr &light : lights) {
+			Utils::Vec3 direction;
+			Light::Type type = light->getType();
+			if (type == Light::Type::DIRECTIONAL) {
+				direction = light->getCoordinates().reverse();
+			}
+			else if (type == Light::Type::POINT) {
+				direction = light->getCoordinates() - point;
+			}
+
+			Ray shadowRay(point, direction, Ray::Type::SHADOW);
+			IntersectionInfo _unused;
+			bool lightOcculded = traceClosest(shadowRay, _unused);
+			// In this case, the ray intersects a shape before reaching the light.
+			if (lightOcculded) {
+				continue;
+			}
+
+			Utils::Vec3 half = ray.getDirection() + shadowRay.getDirection();
+			half = Utils::Operations::normalize(half);
+			finalColor += computeLight(shadowRay.getDirection(), normal, half, light, material);
+		}
+
+		return finalColor;
 	}
 
-	Utils::ColorTriad Raytracer::trace(const Ray &ray) const
+	bool Raytracer::traceClosest(const Ray &ray, IntersectionInfo &info)
 	{
-		Utils::ColorTriad color = Utils::ColorTriad();
-		float closestIntersection = std::numeric_limits<float>::infinity();
 		for (const ShapePtr &shape : shapes) {
 			std::optional<float> optIntersection = shape->intersect(ray);
 			// No intersection between ray and shape.
@@ -23,15 +53,28 @@ namespace Processing
 			}
 
 			float intersection = optIntersection.value();
-			if (intersection < closestIntersection) {
-				closestIntersection = intersection;
-				color.setR(1.0f);
-			}
+			if (intersection < info.t) {
+				// A shadow ray is only concerned if ANY shape is intersected
+				if (ray.getType() == Ray::Type::SHADOW) {
+					return true;
+				}
 
-			//MaterialProps material = shape->getMaterial();
-			//break;
+				info.t = intersection;
+				info.shape = shape;
+			}
 		}
 
-		return color;
+		return info.shape != nullptr;
+	}
+
+	Utils::ColorTriad Raytracer::computeLight(const Utils::Vec3 &direction, const Utils::Vec3 &normal, const Utils::Vec3 &half, const LightPtr &light, const MaterialProps &material)
+	{
+		using namespace Utils;
+		float normalDotDirection = Operations::dot(normal, direction);
+		float normalDotHalf = Operations::dot(normal, half);
+		ColorTriad lightColor = light->getColor();
+		ColorTriad lambert = material.getDiffuse() * lightColor * std::max(normalDotDirection, 0.0f);
+		ColorTriad phong = material.getSpecular() *	lightColor * std::pow(std::max(normalDotHalf, 0.0f), material.getShininess());
+		return lambert + phong;
 	}
 }
