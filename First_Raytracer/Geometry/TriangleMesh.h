625a5aa02123420a95d440f696e49f7b270cf603
Alexander Borgen <alexanderborgen@gmail.com>
2019-07-16 10:06:59 -0700

    TriangleMesh now stores normals

--- First_Raytracer/Geometry/TriangleMesh.h
+++ First_Raytracer/Geometry/TriangleMesh.h
@@ -28,22 +28,29 @@ namespace Geometry
 		const std::vector<TriIndex>& getFaces() const;
 		void setFaces(const std::vector<TriIndex> &faces);
 		void applyTransformation();
+		bool isFinalized();
 
 	public:
 		TriangleMesh() : Shape(Type::TRIANGLE_MESH) {};
 		TriangleMesh(const Utils::Mat4 &transformation, const Processing::MaterialProps &material) : Shape(Type::TRIANGLE_MESH, transformation, material) {};
+		void pushNormal(const Utils::Vec3 &normal);
 		void pushVertex(const Utils::Vec3 &vertex);
 		void commitTriangle(int v0, int v1, int v2);
+		void finalize();
 		std::optional<float> intersect(const Processing::Ray &ray);
-		Utils::Vec3 normalAtPoint(const Utils::Vec3 &point);
+		Utils::Vec3 normalAtPoint(const Processing::Ray &ray, float t);
 
 	private:
+		bool meshFinalized{ false };
+		bool manualNormals{ false };
 		int vertexCount{ 0 };
+		void ensureNormals();
 		std::vector<Utils::Vec3> normals;
 		std::vector<TriIndex> faces;
 		std::vector<Utils::Vec3> vertices;
 		std::shared_ptr<TriIndex> intersectedFace;
 		Processing::BoundingBox boundingBox;
+		void checkState();
 	};
 }
 
--- First_Raytracer/Geometry/src/TriangleMesh.cpp
+++ First_Raytracer/Geometry/src/TriangleMesh.cpp
@@ -9,16 +9,16 @@ namespace Geometry
 		return vertices.at(index);
 	}
 
-	//void TriangleMesh::pushVertex(const Utils::Vec3 &vertex)
-	//{
-	//	Utils::Vec3 transformedVertex = Utils::Operations::vectorTransform(transformation, vertex);
-	//	vertices.push_back(transformedVertex);
-	//	boundingBox.extendBoundary(transformedVertex);
-	//	vertexCount++;
-	//}
+	void TriangleMesh::pushNormal(const Utils::Vec3 &normal)
+	{
+		checkState();
+		normals.push_back(normal);
+		manualNormals = true;
+	}
 
 	void TriangleMesh::pushVertex(const Utils::Vec3 &vertex)
 	{
+		checkState();
 		vertices.push_back(vertex);
 		boundingBox.extendBoundary(vertex);
 		vertexCount++;
@@ -26,10 +26,48 @@ namespace Geometry
 
 	void TriangleMesh::applyTransformation()
 	{
+		checkState();
 		for (Utils::Vec3 &vertex : vertices) {
-			Utils::Vec3 transformedVertex = Utils::Operations::vectorTransform(transformation, vertex);
+			Utils::Vec3 transformedVertex = Utils::Operations::vectorTransform(transformation, vertex, true);
 			vertex = transformedVertex;
 			boundingBox.extendBoundary(transformedVertex);
+		}
+
+		ensureNormals();
+		for (Utils::Vec3 &normal : normals) {
+			normal = Utils::Operations::normalize(Utils::Operations::vectorTransform(normalTransformation, normal, false));
+		}
+	}
+
+	bool TriangleMesh::isFinalized()
+	{
+		return meshFinalized;
+	}
+
+	void TriangleMesh::ensureNormals()
+	{
+		checkState();
+		if (manualNormals) {
+			return;
+		}
+
+		for (const Utils::Vec3 &vertex : vertices) {
+			normals.push_back(Utils::Vec3(0.0f, 0.0f, 0.0f));
+		}
+
+		for (const TriIndex &face : faces) {
+			Utils::Vec3 normalContribution = Triangle(vertices[face.v0], vertices[face.v1], vertices[face.v2]).getNormal();
+			normals.at(face.v0) += normalContribution;
+			normals.at(face.v1) += normalContribution;
+			normals.at(face.v2) += normalContribution;
+		}
+	}
+
+	void TriangleMesh::checkState()
+	{
+		if (isFinalized())
+		{
+			throw new std::exception("Triangle mesh has been finalized, and cannot be changed");
 		}
 	}
 
@@ -45,6 +83,7 @@ namespace Geometry
 
 	void TriangleMesh::setFaces(const std::vector<TriIndex> &faces)
 	{
+		checkState();
 		this->faces = faces;
 	}
 
@@ -55,26 +94,33 @@ namespace Geometry
 
 	void TriangleMesh::commitTriangle(int v0, int v1, int v2)
 	{
+		checkState();
 		faces.push_back(TriIndex{ v0, v1, v2 });
+	}
+
+	void TriangleMesh::finalize()
+	{
+		if (meshFinalized) {
+			return;
+		}
+
+		applyTransformation();
+		meshFinalized = true;
 	}
 
 	std::optional<float> TriangleMesh::intersect(const Processing::Ray &ray)
 	{
+		if (!meshFinalized) {
+			finalize();
+		}
+
 		bool intersectedBox = boundingBox.intersect(ray);
 		if (!intersectedBox) {
 			return std::nullopt;
 		}
 
 		float nearT = std::numeric_limits<float>::infinity();
-		int identicalTriangles = 0;
 		for (const TriIndex &face : faces) {
-			auto o = vertices[face.v0];
-			auto u = vertices[face.v1];
-			auto j = vertices[face.v2];
-
-			if (o == u || o == j || o == u) {
-				identicalTriangles++;
-			}
 			Triangle triangle(vertices[face.v0], vertices[face.v1], vertices[face.v2]);
 			std::optional<float> optIntersection = triangle.intersect(ray);
 			// No intersection between ray and triangle.
@@ -96,14 +142,18 @@ namespace Geometry
 		return nearT;
 	}
 
-	Utils::Vec3 TriangleMesh::normalAtPoint(const Utils::Vec3 &point)
+	Utils::Vec3 TriangleMesh::normalAtPoint(const Processing::Ray &ray, float t)
 	{
 		if (intersectedFace == nullptr) {
 			throw new std::exception("Error: Cannot get normal of intersected triangle if no triangle has been intersected within mesh");
 		}
-
+		// Using Gouraud shading
 		Triangle triangle(vertices[intersectedFace->v0], vertices[intersectedFace->v1], vertices[intersectedFace->v2]);
-		//return Utils::Operations::vectorTransform(normalTransformation, triangle.getNormal());
-		return triangle.getNormal();
+		BarryCentricCoordinates coordinates = triangle.getBarryCentricCoordinates(ray);
+		float u = coordinates[0];
+		float v = coordinates[1];
+		float w = 1.0f - u - v;
+
+		return Utils::Operations::normalize(w * normals[intersectedFace->v0] + u * normals[intersectedFace->v1] + v * normals[intersectedFace->v2]);
 	}
 }
